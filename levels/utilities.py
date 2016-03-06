0fc6ecf456b52b2f5e86e741ef548cf5184f1469
cameron <cameroncairns@gmail.com>
2016-03-05 18:31:08 -0800

    Completes API wrapper, as tested by utilities_test.py

--- levels/utilities.py
+++ levels/utilities.py
@@ -2,7 +2,11 @@ import requests
 
 class APIDaemon:
 
-    def __init__(self, apiKey, account='EXB123456'):
+    def __init__(self, *args, **kwargs):
+        # Gather necessary apiKey and trading account for instantiating object
+        apiKey = kwargs.pop('apiKey')
+        account = kwargs.pop('account', 'EXB123456')
+        # Test that input is in valid format
         assert(all((isinstance(apiKey, str), isinstance(account, str))))
         self.account = account
         self.apiKey = apiKey
@@ -20,21 +24,52 @@ class APIDaemon:
 
 class StockDaemon(APIDaemon): 
 
-    def __init__(self, venue, stock, *args, **kwargs):
-        assert(all((isinstance(venue, str), isinstance(stock, str))))
+    def __init__(self, *args, **kwargs):
         super(StockDaemon, self).__init__(*args, **kwargs)
-        self.venue = venue
-        self.stock = stock
-        URLS = {
-            'stock_orderbook': "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}".format(venue, stock),
-            'stock_quote': "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/quote".format(venue, stock),
-            }
+        self.venue = kwargs.pop('venue')
+        self.stock = kwargs.pop('stock')
+        self.orders = {}
+
+    def __str__(self):
+        return "{0}: {1}".format(self.venue, self.stock)
+
+    def cancel_order(self, order):
+        assert(isinstance(order, int))
+        return self._post_to_api(
+                "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders/{2}/cancel".format(self.venue, self.stock, order)
+                )
+
+    def get_order_status(self, order_id=None):
+        if order_id:
+            # User has defined a specific order to check the status for
+            assert(isinstance(order_id, int))
+            data = self._get_from_api(
+                    "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders/{2}".format(self.venue, self.stock, order_id)
+                    )
+            return data if data.pop('ok') else data.pop('error', 'No error Specified')
+        else:
+            # No order id specified so return all orders for this stock on this venue
+            data = self._get_from_api(
+                    "https://api.stockfighter.io/ob/api/venues/{0}/accounts/{1}/orders".format(self.venue, self.account)
+                    ).json()
+            return data if data.pop('ok') else data.pop('error', 'No error Specified')
+
 
     def get_orderbook(self):
-        data = requests.get(self.apiUrls[request]).json()
+        data = self._get_from_api(
+                "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}".format(self.venue, self.stock)
+                ).json()
+        return({'bids': data['bids'], 'asks': data['asks']} if data['ok'] 
+                 else data.pop('error', 'No error Specified'))
+
+    def get_stock_quote(self):
+        data = self._get_from_api(
+                "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/quote".format(self.venue, self.stock)
+                ).json()
         return data if data.pop('ok') else data.pop('error', 'No error Specified')
 
-    def post_order(price=0,
+    def post_order(self,
+                   price=0,
                    quantity=100,
                    direction='buy',
                    orderType='market'):
@@ -46,8 +81,8 @@ class StockDaemon(APIDaemon):
                     )
                    )
                )
-        response= _post_to_api(
-                "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders".format(venue, stock),
+        data = self._post_to_api(
+                "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders".format(self.venue, self.stock),
                 {'account': self.account,
                  'venue': self.venue,
                  'stock': self.stock,
@@ -55,70 +90,56 @@ class StockDaemon(APIDaemon):
                  'qty': quantity,
                  'direction': direction,
                  'orderType': orderType,
-                 })
-        return filter(lambda x: x.pop(['ok']), response.json())
-
-    def delete_order(self, order):
-        assert(isinstance(order, int))
-        return _post_to_api(
-                "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders/{2}/cancel".format(self.venue, self.stock, order)
-                )
-
-    def get_order_status(self, order):
-        assert(isinstance(order, int))
-        return _get_from_api(
-                "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders/{2}".format(self.venue, self.stock, order)
-                )
-
-    def get_all_order_status(self):
-        assert(isinstance(order, int))
-        response = _get_from_api(
-                "https://api.stockfighter.io/ob/api/venues/{0}/stocks/{1}/orders".format(self.venue, self.stock)
-                ).json()
-
-    def get_stock_quote(venue='TESTEX', stock='FOOBAR'):
-        assert(all((isinstance(venue, str), isinstance(stock, str))))
-        response = requests.get(url)
-        return filter(lambda x: bool(x.pop(['ok'])), response.json())
+                 }).json()
+        return data if data.pop('ok') else data.pop('error', 'No error Specified')
 
 class AccountDaemon(APIDaemon):
 
     def __init__(self, *args, **kwargs):
         super(AccountDaemon, self).__init__(*args, **kwargs)
-        self.stockdaemons = {}
+        self.stock_daemons = {}
 
     def _api_up(self):
        return self._get_from_api("https://api.stockfighter.io/ob/api/heartbeat")
 
-    def _get_venue_stocks(self,venue):
-        return [ stock for stock['symbol'] in self._get_from_api(
+    def _get_venue_stocks(self, venue):
+        return [ stock['symbol'] for stock in self._get_from_api(
                 "https://api.stockfighter.io/ob/api/venues/{0}/stocks".format(venue)
                 ).json()['symbols'] ]
 
-    def _get_venue_orders(self, venue):
-        return self._get_from_api(
-                "https://api.stockfighter.io/ob/api/venues/{0}/accounts/{1}/orders".format(venue, account)
-                ).json()
-
     def _venue_up(self, venue):
         return self._get_from_api(
-                "https://api.stockfighter.io/ob/api/venues/{0}/stocks".format(venue),
+                "https://api.stockfighter.io/ob/api/venues/{0}/stocks".format(venue)
                 )
 
-    def spawn_daemons(self, venue='TESTEX'):
+    def get_all_order_status(self, venue=None):
+        if venue:
+            data = self._get_from_api(
+                        "https://api.stockfighter.io/ob/api/venues/{0}/accounts/{1}/orders".format(venue, self.account)
+                        ).json()
+            return data if data.pop('ok') else data.pop('error', 'No error Specified')
+        else:
+            venue_statuses = {}
+            for venue_name in self.stock_daemons.keys():
+                data = self._get_from_api(
+                            "https://api.stockfighter.io/ob/api/venues/{0}/accounts/{1}/orders".format(venue_name, self.account)
+                            ).json()
+                venue_statuses[venue_name] = data['orders'] if data.pop('ok') else data.pop('error', 'No error Specified')
+            return venue_statuses
+
+    def spawn_stock_daemons(self, venue='TESTEX'):
+        assert(isinstance(venue, str))
         if(not self._api_up() or not self._venue_up(venue)):
             # Check that the API and Venue site status is up
             return None
         # initialize a new dictionary to hold all the stockdaemons
-        self.stockdaemons[venue] = {}
+        self.stock_daemons[venue] = {}
         for stock in self._get_venue_stocks(venue):
             # Create a stockdaemon for every valid stock in venue
-            self.stockdaemons[venue][stock] = StockDaemon(
+            assert(isinstance(stock, str))
+            self.stock_daemons[venue][stock] = StockDaemon(
                                                     apiKey=self.apiKey,
                                                     account=self.account,
                                                     venue=venue,
                                                     stock=stock,
                                                     )
-
-    def check_all_orders_for_venue(venue):
-        return [ order for order in self._get_venue_orders(venue) ]
--- levels/utility_test.py
+++ levels/utility_test.py
@@ -1,6 +1,18 @@
 import pdb
 
-import utilities
+from utilities import AccountDaemon
 
-response = utilities.post_order('a842566b1b9e4a53fd5a29cff3729ec4790b7823')
 pdb.set_trace()
+
+test_account = AccountDaemon(apiKey='a842566b1b9e4a53fd5a29cff3729ec4790b7823')
+
+test_account.spawn_stock_daemons()
+
+for stock_daemon in test_account.stock_daemons['TESTEX'].values():
+    stock_daemon.get_orderbook() 
+    stock_daemon.get_stock_quote() 
+    order = stock_daemon.post_order() 
+    stock_daemon.get_order_status() 
+    stock_daemon.cancel_order(order['id']) 
+
+test_account.get_all_order_status()
